cca5497c1a741da9ede904a0b26bc2edd6f1b9b4
Arunangshu Biswas <arunangshubsws@gmail.com>
2019-10-14 23:22:51 +0530

    Added exception handling
    Signed-off-by: Arunangshu Biswas <arunangshubsws@gmail.com>

--- faserver/admin/routes.py
+++ faserver/admin/routes.py
@@ -19,17 +19,20 @@ def add_user():
         last_name = request.form['last_name']
         uploaded_files = request.files.getlist("images")
 
-        # try:
-        add_user_entry(first_name, last_name)
+        try:
+            if len(uploaded_files) != 10:
+                raise Exception('[ERROR] Correct number of files not received!')
 
-        foldername = "{0}_{1}".format(first_name, last_name)
-        add_to_imgdb(foldername, uploaded_files)
+            add_user_entry(first_name, last_name)
 
-        retrain_svc()
+            foldername = "{0}_{1}".format(first_name, last_name)
+            add_to_imgdb(foldername, uploaded_files)
 
-        return redirect('/admin/add-user')
-        # except:
-        #     return '[ERROR] There was a problem adding that user!'
+            retrain_svc()
+
+            return redirect('/admin/add-user')
+        except:
+            return '[ERROR] There was a problem adding that user!'
     else:
         return render_template('admin/add_user.html')
 
@@ -44,20 +47,23 @@ def edit_user():
         old_last_name = request.form['old_last_name']
         uploaded_files = request.files.getlist("images")
         
-        # try:
-        edit_user_entry(id, first_name, last_name)
+        try:
+            if len(uploaded_files) != 10:
+                raise Exception('[ERROR] Correct number of files not received!')
 
-        old_folder_name = "{0}_{1}".format(old_first_name, old_last_name)
-        delete_from_imgdb(old_folder_name)
+            edit_user_entry(id, first_name, last_name)
 
-        new_folder_name = "{0}_{1}".format(first_name, last_name)
-        add_to_imgdb(new_folder_name, uploaded_files)
+            old_folder_name = "{0}_{1}".format(old_first_name, old_last_name)
+            delete_from_imgdb(old_folder_name)
 
-        retrain_svc()
+            new_folder_name = "{0}_{1}".format(first_name, last_name)
+            add_to_imgdb(new_folder_name, uploaded_files)
 
-        return redirect('/admin/edit-user')
-        # except:
-        #     return '[ERROR] There was a problem editing that user!'
+            retrain_svc()
+
+            return redirect('/admin/edit-user')
+        except:
+            return '[ERROR] There was a problem editing that user!'
     else:
         users = User.query.order_by(User.date_registered).all()
         return render_template('admin/edit_user.html', users=users)
@@ -69,18 +75,18 @@ def delete_user():
         id = request.form['user_id']
         user = User.query.get_or_404(id)
 
-        # try:
-        # Delete user entry from database
-        delete_user_entry(user)
+        try:
+            # Delete user entry from database
+            delete_user_entry(user)
 
-        foldername = "{0}_{1}".format(user.first_name, user.last_name)
-        delete_from_imgdb(foldername)
+            foldername = "{0}_{1}".format(user.first_name, user.last_name)
+            delete_from_imgdb(foldername)
 
-        retrain_svc()
+            retrain_svc()
 
-        return redirect('/admin/delete-user')
-        # except:
-        #     return '[ERROR] There was a problem deleting that user!'
+            return redirect('/admin/delete-user')
+        except:
+            return '[ERROR] There was a problem deleting that user!'
     else:
         users = User.query.order_by(User.date_registered).all()
         return render_template('admin/delete_user.html', users=users)
