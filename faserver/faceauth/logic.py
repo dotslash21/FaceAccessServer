dc08b69aab5dc865c2e794d9cb28c507b22c8195
Arunangshu Biswas <arunangshubsws@gmail.com>
2019-10-31 16:47:20 +0530

    Initial faceauth face id pipeline setup
    Signed-off-by: Arunangshu Biswas <arunangshubsws@gmail.com>

--- faserver/faceauth/logic.py
+++ faserver/faceauth/logic.py
@@ -1,6 +1,8 @@
 import hashlib
+from .. import app
 from ..models import Camera
 from ..extensions import db
+from ..utils.face_id import FaceIdentifier
 
 
 def getToken(string):
@@ -21,3 +23,32 @@ def add_camera_entry(camera_name, camera_serial_num, camera_token):
     except BaseException:
         raise Exception(
             '[ERROR] Problem encountered while adding the camera entry to database!')
+
+
+def face_recognition(frame):
+    # Initialize the FaceIdentifier class
+    face_id = FaceIdentifier(
+        app.config['ALIGNED_IMG_DB'],
+        app.config['MTCNN_MODEL_DIR'],
+        app.config['FACENET_PRETRAINED_MODEL_PATH'],
+        app.config['SVC_CLASSIFIER_SAVE_PATH']
+    )
+
+    if app.config['SVC_RELOAD']:
+        face_id.load_svc()
+        app.config['SVC_RELOAD'] = False
+
+    id_result = face_id.identify(frame)
+
+    # Catch any errors in identification
+    if not isinstance(id_result, type(int())):
+        # Get the detection results and bounding box
+        (faceID, _, detection_probability) = id_result
+        # Set the detection result variable
+        detection_result = {'status': 'PASS',
+                            'id': faceID,
+                            'probability': detection_probability}
+    else:
+        detection_result = {'status': 'FAIL'}
+
+    return detection_result
--- faserver/faceauth/routes.py
+++ faserver/faceauth/routes.py
@@ -1,31 +1,82 @@
-from .logic import getToken, add_camera_entry
+import base64
+from .logic import getToken, add_camera_entry, face_recognition
 from flask import Blueprint, request, jsonify
 
 mod = Blueprint("faceauth", __name__, template_folder="templates")
 
 
-@mod.route("/camera-register", methods=['POST'])
+@mod.route("/register-client", methods=['POST'])
 def camera_register():
     if request.method == 'POST':
-        camera_name = request.data['name']
-        camera_serial_num = request.data['serial_number']
+        camera_name = request.data['clientId']
+        camera_serial_num = request.data['serialNumber']
         camera_token = getToken(camera_name + camera_serial_num)
 
         try:
             add_camera_entry(camera_name, camera_serial_num, camera_token)
 
             response_data = {
+                'status': 'PASS',
                 'message': "Camera registered succesfully!",
                 'token': camera_token
             }
             return jsonify(response_data)
         except BaseException:
             response_data = {
+                'status': 'FAIL',
                 'message': "There was a problem registering the camera"
             }
             return jsonify(response_data)
     else:
         response_data = {
+            'status': 'FAIL',
             'message': "Invalid request!"
         }
         return jsonify(response_data)
+
+
+@mod.route("/face-auth", methods=['POST'])
+def face_auth():
+    if request.method == 'POST':
+        encoded_img_list = list(request.data["imageList"])
+        id_dict = {}
+
+        for encoded_image in encoded_img_list:
+            frame = base64.decodestring(encoded_image)
+
+            result = face_recognition(frame)
+
+            if result['status'] == 'PASS':
+                id = result['id']
+                probability = result['probability']
+
+                if id in id_dict:
+                    id_dict['id']['probability_sum'] += probability
+                    id_dict['id']['count'] += 1
+                else:
+                    id_dict['id'] = {
+                        'name': id.replace('_', " "),
+                        'probability_sum': probability,
+                        'count': 1
+                    }
+
+        finalId = ""
+        finalName = ""
+        finalProbability = 0
+        for id, data in id_dict.items():
+            if data['probability_sum'] / data['count'] > finalProbability:
+                finalProbability = ['probability_sum'] / data['count']
+                finalName = data['name']
+                finalId = id
+
+        return jsonify({
+            'status': 'PASS',
+            'id': finalId,
+            'name': finalName,
+            'probability': probability
+        })
+    else:
+        return jsonify({
+            'status': 'FAIL',
+            'message': 'Invalid request!'
+        })
