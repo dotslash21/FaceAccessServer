ba351c795ce6246dc709aad409c998db68eb4e4f
Arunangshu Biswas <arunangshubsws@gmail.com>
2019-11-06 19:04:11 +0530

    Updated pretrained model from davidsandberg's repo
    Signed-off-by: Arunangshu Biswas <arunangshubsws@gmail.com>

--- faserver/config.py
+++ faserver/config.py
@@ -7,7 +7,7 @@ ALLOWED_IMG_EXTENSIONS = set(["png", "jpg", "jpeg", "gif"])
 SQLALCHEMY_DATABASE_URI = "sqlite:///" + \
     BASE_PATH + "/faserver/database/users.db"
 FACENET_PRETRAINED_MODEL_PATH = BASE_PATH + \
-    "/faserver/utils/pretrained_model/20170511-185253.pb"
+    "/faserver/utils/pretrained_model/20180402-114759/20180402-114759.pb"
 MTCNN_MODEL_DIR = BASE_PATH + "/faserver/utils/npy"
 SVC_CLASSIFIER_SAVE_PATH = BASE_PATH + "/faserver/utils/svc_classifier/SVC.pkl"
 SVC_RELOAD = False
